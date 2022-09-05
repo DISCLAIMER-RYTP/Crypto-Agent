7ea1c7b4cec9698dcddc66434b9ea20b7aef161c
Ernendo <55671896+Densvir33@users.noreply.github.com>
2022-09-05 15:13:46 +0300

    aboba v2

--- CryptoAgent/crypto-agent/src/Main.js
+++ CryptoAgent/crypto-agent/src/Main.js
@@ -1,36 +1,16 @@
 import React from 'react'
-import {useState} from 'react'
 import './Main.css'
 import Header2 from './Header2.js'
-import BarChart from './Diag1'
-import {UserData} from './Data'
 
 export default function Main() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-      label: "BTC",
-      data: UserData.map((data) => data.userGain),
-      backgroundColor: [
-        "rgb(125, 125, 125)"
-      ],
-      borderColor: "black",
-      borderWidth: 2,
-    },
-    ],
-  });
   return (
-    <div className='mm'>
+    <div>
       <Header2/>
+      <div></div>
       <div>
-        <div style={{width: 600}}>
-        <BarChart chartData={userData}/>
-        </div>
         <div></div>
         <div></div>
       </div>
     </div>
   )
 }
-
